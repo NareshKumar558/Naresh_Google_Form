d62a903478477d5c3b01af7c9fc4bbeb247a5daa
beheranareshkumar558 <beheranareshkumar558@gmail.com>
2024-08-01 00:05:06 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,6 +1,7 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,25 +11,129 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
     ChromeDriver driver;
+    private Wrappers wrappers;
 
     /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
+     * TODO: Write your tests here with testng @Test annotation.
+     * Follow `testCase01` `testCase02`... format or what is provided in
+     * instructions
      */
 
-     
+    @Test
+    public void testCase01() {
+        try {
+            System.out.println("Start Test case: testCase01");
+            // hit url
+            driver.get(
+                    "https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+            Thread.sleep(2000);
+            // String CurrentURL=driver.getCurrentUrl();
+            String x = this.wrappers.getUrl();
+            String y = "forms";
+            if (x.contains(y)) {
+                System.out.println("URL is correct");
+            } else {
+                System.out.println("URL is not correct");
+            }
+
+            //name
+            WebElement name = wrappers.elementSearch("//input[@aria-describedby='i2 i3']");
+            //send name
+            wrappers.inputText(name, "Crio Learner");
+
+            //practicing Automation?
+            WebElement pauto = wrappers.elementSearch("//textarea[@aria-describedby='i6 i7']");
+            //send practicing Automation?
+            wrappers.inputText(pauto, "I want to be the best QA Engineer! 1710572021");
+
+            //experience
+            WebElement experience= wrappers.elementSearch("//div[@id='i13']");
+            //click option
+            wrappers.select(experience);
+
+            //java
+            WebElement java= wrappers.elementSearch("//div[@id='i30']");
+            //click option
+            wrappers.select(java);
+
+            //selenium
+            WebElement selenium= wrappers.elementSearch("//div[@id='i33']");
+            //click option
+            wrappers.select(selenium);
+
+            //testNG
+            WebElement testNG= wrappers.elementSearch("//div[@id='i39']");
+            //click option
+            wrappers.select(testNG);
+
+            //dropdown
+            WebElement dropdown= wrappers.elementSearch("(//div[@jsname='wQNmvb'])[1]");
+            //click option
+            wrappers.select(dropdown);
+
+            Thread.sleep(1000);
+
+            //select Mr  
+            WebElement mr= wrappers.elementSearch("(//div[@data-value='Mr'])[2]");
+            //click option
+            wrappers.select(mr);
+
+            //date  
+            WebElement date= wrappers.elementSearch("//input[@type='date']");
+            //send date
+            wrappers.inputTextDate(date,"24-07-2024");
+
+            //hr time
+            WebElement hr= wrappers.elementSearch("//*[@id=\"mG61Hd\"]/div[2]/div/div[2]/div[7]/div/div/div[2]/div/div[1]/div[2]/div[1]/div/div[1]/input");
+            //send time
+            wrappers.inputTextDate(hr,"07");
+
+            //min time
+            WebElement min= wrappers.elementSearch("//*[@id=\"mG61Hd\"]/div[2]/div/div[2]/div[7]/div/div/div[2]/div/div[3]/div/div[1]/div/div[1]/input");
+            //send time
+            wrappers.inputTextDate(min,"30");
+
+            //submit
+            WebElement submit= wrappers.elementSearch("//*[@id=\"mG61Hd\"]/div[2]/div/div[3]/div[1]/div[1]/div");
+            //send time
+            wrappers.select(submit);
+
+
+            Thread.sleep(5000);
+
+            //get success message
+            WebElement message= wrappers.elementSearch("//div[@class='vHW8K']");
+            //send time
+            String mess=wrappers.getMessage(message);
+            if(mess.contains("Thanks for your response, Automation Wizard!")){
+                System.out.println("Correct message");
+            }
+            else{
+                System.out.println("Incorrect message");
+            }
+
+            System.out.println("end Test case: testCase01");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("TestCase01 Failed!");
+        }
+
+    }
+
     /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
+     * Do not change the provided methods unless necessary, they will help in
+     * automation and assessment
      */
     @BeforeTest
-    public void startBrowser()
-    {
+    public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
@@ -42,16 +147,19 @@ public class TestCases {
         options.setCapability("goog:loggingPrefs", logs);
         options.addArguments("--remote-allow-origins=*");
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
         driver = new ChromeDriver(options);
 
+        // might have to change
+        wrappers = new Wrappers(driver);
+
+        //driver.manage().timeouts().PageLoadTimeOut(Duration.ofSeconds(30));
         driver.manage().window().maximize();
     }
 
     @AfterTest
-    public void endTest()
-    {
+    public void endTest() {
         driver.close();
         driver.quit();
 
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -7,7 +7,10 @@ import java.time.format.DateTimeFormatter;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
@@ -16,4 +19,31 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    public Wrappers(WebDriver driver){
+        this.driver=driver;
+    }
+
+    public String getUrl(){
+        return this.driver.getCurrentUrl();
+
+    }
+    public WebElement elementSearch(String x){
+        return this.driver.findElement(By.xpath(x));
+    }
+    public void inputText(WebElement input,String x){
+        input.sendKeys(x);
+    }
+    public void select(WebElement input){
+        input.click();
+    }
+    public void inputTextDate(WebElement input, String x){
+        input.sendKeys(x);
+    }
+    public String getMessage(WebElement input){
+        return input.getText();
+    }
+    
 }
